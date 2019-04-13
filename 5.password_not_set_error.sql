9c4beb6f6c4d02d0263c8917cf796c7d6d7922ed
fduan0625 <40590671+fduan0625@users.noreply.github.com>
2019-04-12 18:10:31 -0700

    Update 5.password_not_set_error.sql

--- 5.password_not_set_error.sql
+++ 5.password_not_set_error.sql
@@ -9,7 +9,7 @@ from dynecom_execution_events
 where dateint=20190323
 and other_properties['input.step'] = 'SIGNIN'
 and other_properties['input.visitor_state']='NON_REGISTERED_MEMBER'
-)
+);
 
 select 
 input_flow
@@ -27,3 +27,60 @@ left join dynecom_execution_events dn2
 	 and dn2.dateint between 20190323 and 20190324
 	 and dn2.other_properties['output.visitor_state']='CURRENT_MEMBER'
 group by 1;
+		
+
+select 
+case when su.acct_id is not null then 1 else 0 end as signup_t_f
+,case when setpw.acct_id is not null then 1 else 0 end as setup_pw_t_f
+,setpw.member_status
+,count(distinct reg.acct_id) as num_distinct_register
+,count(distinct su.acct_id) as num_signup
+,count(distinct setpw.acct_id) as num_setup_pw
+
+from
+
+	(
+	select other_properties['row.values.account_id'] as acct_id
+	,other_properties['row.values.membership_status'] as member_status
+	,event_utc_ms
+	,dateint
+
+	from subscriber2_update_logs 
+	where other_properties['operation_name']='CreateAccount'
+	and restricted_do_not_copy['row.values.password_status']='EMPTY'
+	and dateint between 20190401 and 20190407
+	) reg
+left join 
+	(
+	select other_properties['row.values.account_id'] as acct_id
+	,other_properties['row.values.membership_status'] as member_status
+	,event_utc_ms
+	,dateint
+
+	from subscriber2_update_logs 
+	where other_properties['operation_name']='StartMembership'
+	-- and restricted_do_not_copy['row.values.password_status']='EMPTY'
+	and dateint >=  20190401 
+	) su
+	on reg.acct_id = su.acct_id
+	   and reg.dateint <= su.dateint
+
+left join
+	(
+	select other_properties['row.values.account_id'] as acct_id
+	,other_properties['row.values.membership_status'] as member_status
+	,event_utc_ms
+	,dateint
+
+	from subscriber2_update_logs 
+	where other_properties['operation_name']='SetPassword'
+	and restricted_do_not_copy['row.before_values.password_status']='EMPTY'
+	and restricted_do_not_copy['row.values.password_status']='VALID'
+	and dateint >=  20190401
+	) setpw
+
+	on reg.acct_id = setpw.acct_id
+	   and reg.dateint <= setpw.dateint
+
+group by 1,2,3;		
+		
